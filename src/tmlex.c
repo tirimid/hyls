f5076bbc573e6b1ce5ef63d13f2403d0fc5c47da
dmfedorin <m7996@students.eischools.ae>
2022-06-17 11:09:58 +0400

    lexer now works properly

--- src/tmlex.c
+++ src/tmlex.c
@@ -14,13 +14,43 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
 {
     int next_tok = 0;
     int line     = 1;
-    
+
+    // extract all tokens, keywords are not processed here
     for (const char *c = src; c < src + src_len; ++c)
     {
-        if (strchr(" \t\r\f", *c))
+        if (strchr(" \t\r\f\0", *c))
             continue;
         else if (*c == '\n')
             ++line;
+        else if (strchr(":;={}", *c)) // special characters
+        {
+            enum tmlex_token_type tok_type;
+            switch (*c)
+            {
+            case ':':
+                tok_type = TMLEX_TOKEN_TYPE_COLON;
+                break;
+            case ';':
+                tok_type = TMLEX_TOKEN_TYPE_SEMICOLON;
+                break;
+            case '=':
+                tok_type = TMLEX_TOKEN_TYPE_EQUALS;
+                break;
+            case '{':
+                tok_type = TMLEX_TOKEN_TYPE_BRACE_LEFT;
+                break;
+            case '}':
+                tok_type = TMLEX_TOKEN_TYPE_BRACE_RIGHT;
+                break;
+            }
+
+            out_toks[next_tok] = (struct tmlex_token)
+            {
+                .type = tok_type,
+                .line = line,
+            };
+            ++next_tok;
+        }
         else if (isdigit(*c)) // literal number
         {
             if (next_tok >= max_tok_cnt)
@@ -31,6 +61,7 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
 
             for (; isdigit(*c) || *c == '.'; ++c)
                 strncat(lex_buf, c, 1);
+            --c; // on next loop iteration, a character is skipped if this is not done
 
             float num = (float)atof(lex_buf);
             free(lex_buf);
@@ -51,8 +82,10 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
             char *lex_buf = malloc(512);
             memset(lex_buf, '\0', sizeof(lex_buf));
 
+            ++c; // not doing this will instantly terminate the string
             for (; *c != '"'; ++c)
                 strncat(lex_buf, c, 1);
+            // not decrementing c here, closing quote should be skipped
 
             char *str_buf = malloc(strlen(lex_buf) + 1);
             strcpy(str_buf, lex_buf);
@@ -60,9 +93,9 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
             
             out_toks[next_tok] = (struct tmlex_token)
             {
-                .type    = TMLEX_TOKEN_TYPE_LITERAL_STRING,
-                .line    = line,
-                .val.str = str_buf,
+                .type     = TMLEX_TOKEN_TYPE_LITERAL_STRING,
+                .line     = line,
+                .val.text = str_buf,
             };
             ++next_tok;
         }
@@ -76,6 +109,7 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
 
             for (; isalnum(*c) || *c == '_'; ++c)
                 strncat(lex_buf, c, 1);
+            --c; // on next loop iteration, a character is skipped if this is not done
 
             char *iden_buf = malloc(strlen(lex_buf) + 1);
             strcpy(iden_buf, lex_buf);
@@ -85,15 +119,49 @@ void tmlex_lex(struct tmlex_token out_toks[], int *out_tok_cnt, int max_tok_cnt,
             {
                 .type     = TMLEX_TOKEN_TYPE_IDENTIFIER,
                 .line     = line,
-                .val.iden = iden_buf,
+                .val.text = iden_buf,
             };
             ++next_tok;
         }
         else
             UTIL_PRINT_ERRORF("tmlex encountered invalid character '%c' on line %d", *c, line);
     }
+
+    // process keywords (which are currently stored as identifiers)
+    for (int i = 0; i < next_tok; ++i)
+    {
+        struct tmlex_token *tok = &out_toks[i];
+        if (tok->type == TMLEX_TOKEN_TYPE_IDENTIFIER)
+        {
+            if (strcmp(tok->val.text, "category") == 0)
+            {
+                free(tok->val.text);
+                tok->type = TMLEX_TOKEN_TYPE_KEYWORD_CATEGORY;
+            }
+            else if (strcmp(tok->val.text, "list") == 0)
+            {
+                free(tok->val.text);
+                tok->type = TMLEX_TOKEN_TYPE_KEYWORD_LIST;
+            }
+            else if (strcmp(tok->val.text, "item") == 0)
+            {
+                free(tok->val.text);
+                tok->type = TMLEX_TOKEN_TYPE_KEYWORD_ITEM;
+            }
+            else if (strcmp(tok->val.text, "num") == 0)
+            {
+                free(tok->val.text);
+                tok->type = TMLEX_TOKEN_TYPE_KEYWORD_NUM;
+            }
+            else if (strcmp(tok->val.text, "str") == 0)
+            {
+                free(tok->val.text);
+                tok->type = TMLEX_TOKEN_TYPE_KEYWORD_STR;
+            }
+        }
+    }
     
-    *out_tok_cnt = next_tok <= 1 ? 0 : next_tok - 1;
+    *out_tok_cnt = next_tok;
 }
 
 void tmlex_print_tokens(const struct tmlex_token toks[], int tok_cnt)
@@ -103,14 +171,46 @@ void tmlex_print_tokens(const struct tmlex_token toks[], int tok_cnt)
         printf("[%d] ", i);
         switch (toks[i].type)
         {
+        case TMLEX_TOKEN_TYPE_COLON:
+            printf("colon (:)\n");
+            break;
+        case TMLEX_TOKEN_TYPE_SEMICOLON:
+            printf("semicolon (;)\n");
+            break;
+        case TMLEX_TOKEN_TYPE_EQUALS:
+            printf("equals (=)\n");
+            break;
+        case TMLEX_TOKEN_TYPE_BRACE_LEFT:
+            printf("lbrace ({)\n");
+            break;
+        case TMLEX_TOKEN_TYPE_BRACE_RIGHT:
+            printf("rbrace (})\n");
+            break;
+
+        case TMLEX_TOKEN_TYPE_KEYWORD_CATEGORY:
+            printf("kwcat\n");
+            break;
+        case TMLEX_TOKEN_TYPE_KEYWORD_LIST:
+            printf("kwlist\n");
+            break;
+        case TMLEX_TOKEN_TYPE_KEYWORD_ITEM:
+            printf("kwitem\n");
+            break;
+        case TMLEX_TOKEN_TYPE_KEYWORD_NUM:
+            printf("kwnum\n");
+            break;
+        case TMLEX_TOKEN_TYPE_KEYWORD_STR:
+            printf("kwstr\n");
+            break;
+            
         case TMLEX_TOKEN_TYPE_LITERAL_NUMBER:
             printf("litnum: %f\n", toks[i].val.num);
             break;
         case TMLEX_TOKEN_TYPE_LITERAL_STRING:
-            printf("litstr: \"%s\"\n", toks[i].val.str);
+            printf("litstr: \"%s\"\n", toks[i].val.text);
             break;
         case TMLEX_TOKEN_TYPE_IDENTIFIER:
-            printf("iden: %s\n", toks[i].val.iden);
+            printf("iden: %s\n", toks[i].val.text);
             break;
         }
     }
@@ -118,4 +218,14 @@ void tmlex_print_tokens(const struct tmlex_token toks[], int tok_cnt)
 
 void tmlex_free_tokens(struct tmlex_token toks[], int tok_cnt)
 {
+    for (int i = 0; i < tok_cnt; ++i)
+    {
+        switch (toks[i].type)
+        {
+        case TMLEX_TOKEN_TYPE_LITERAL_STRING:
+        case TMLEX_TOKEN_TYPE_IDENTIFIER:
+            free(toks[i].val.text);
+            break;
+        }
+    }
 }
